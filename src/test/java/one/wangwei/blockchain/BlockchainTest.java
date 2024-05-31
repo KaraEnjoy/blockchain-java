d2c3e0ac5de856b487df9a1cd67e864e0fe2e754
Krest <18635572970@163.com>
2024-05-31 11:44:47 +0800

    [M] rockDb 版本

--- src/test/java/one/wangwei/blockchain/BlockchainTest.java
+++ src/test/java/one/wangwei/blockchain/BlockchainTest.java
@@ -15,27 +15,6 @@ import org.junit.Test;
 public class BlockchainTest {
 
 
-    @Test
-    public void test1() throws Exception {
-
-        Blockchain blockchain = Blockchain.newBlockchain();
-
-        blockchain.addBlock("Send 1 BTC to Ivan");
-        blockchain.addBlock("Send 2 more BTC to Ivan");
-
-        Blockchain.BlockchainIterator blockchainIterator = blockchain.getBlockchainIterator();
-        while (blockchainIterator.hashNext()){
-            Block block = blockchainIterator.next();
-            System.out.println("Prev.hash: " + block.getPrevBlockHash());
-            System.out.println("Data: " + block.getData());
-            System.out.println("Hash: " + block.getHash());
-            System.out.println("Nonce: " + block.getNonce());
-            ProofOfWork pow = ProofOfWork.newProofOfWork(block);
-            System.out.println("Pow valid: " + pow.validate() + "\n");
-        }
-
-
-    }
 
     public static void main(String[] args) {
         try {
