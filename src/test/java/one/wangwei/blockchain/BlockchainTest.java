7121f089d43a1390d4462735a0f86c6c182f937d
Krest <18635572970@163.com>
2024-05-31 10:29:37 +0800

    [A] 增加测试数据

--- src/test/java/one/wangwei/blockchain/BlockchainTest.java
+++ src/test/java/one/wangwei/blockchain/BlockchainTest.java
@@ -1,6 +1,10 @@
 package one.wangwei.blockchain;
 
+import one.wangwei.blockchain.block.Block;
+import one.wangwei.blockchain.block.Blockchain;
 import one.wangwei.blockchain.cli.CLI;
+import one.wangwei.blockchain.pow.ProofOfWork;
+import org.junit.Test;
 
 /**
  * 测试
@@ -9,6 +13,29 @@ import one.wangwei.blockchain.cli.CLI;
  * @date 2018/02/05
  */
 public class BlockchainTest {
+
+
+    @Test
+    public void test1() throws Exception {
+
+        Blockchain blockchain = Blockchain.newBlockchain();
+
+        blockchain.addBlock("Send 1 BTC to Ivan");
+        blockchain.addBlock("Send 2 more BTC to Ivan");
+
+        Blockchain.BlockchainIterator blockchainIterator = blockchain.getBlockchainIterator();
+        while (blockchainIterator.hashNext()){
+            Block block = blockchainIterator.next();
+            System.out.println("Prev.hash: " + block.getPrevBlockHash());
+            System.out.println("Data: " + block.getData());
+            System.out.println("Hash: " + block.getHash());
+            System.out.println("Nonce: " + block.getNonce());
+            ProofOfWork pow = ProofOfWork.newProofOfWork(block);
+            System.out.println("Pow valid: " + pow.validate() + "\n");
+        }
+
+
+    }
 
     public static void main(String[] args) {
         try {
